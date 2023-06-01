223ec52b1a2bb50daac24882132bdb252b9ea133
majidsh1992 <121107930+majidsh1992@users.noreply.github.com>
2023-06-01 19:04:49 +0430

    game-of-human-007
    ساخت یک صفحه html , css برای پروژه ی بازی انفجار

---
+++ app.js
@@ -0,0 +1,7 @@
+let body =document.querySelector("body")
+let section = document.querySelector("section")
+let container = document.querySelector("#container")
+let hummy = document.querySelector("#hummy")
+let bomb = document.querySelector("#bomb")
+
+
