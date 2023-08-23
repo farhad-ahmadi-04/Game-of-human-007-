d70756578f43a7afbfc1f8f52097cd3912372392
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-08-23 10:18:16 +0330

    update
    فانکش ها به فانکش های کوچیک تر تقسیم شدن
    
    فایل جی اس دو تا صفحه بهم لینک شد ک بشه فانکش ها رو از ی فایل دیگ صدا زد

--- alireza-chooseSide/app.js
+++ alireza-chooseSide/app.js
@@ -1,59 +1,65 @@
 // 1.get input value and save it in local storage............
 
-  // when playnow button is clicked
+// when playnow button is clicked
 let button = document.querySelector("button");
 button.addEventListener("click", (e) => {
-    // stop refresh & get values
-    e.preventDefault();
+  // stop refresh & get values
+  e.preventDefault();
 
-// input values > player 1 & 2 > name & number
-let nameP1V = document.querySelector("#nameP1V").value;
-let numberP1V = document.querySelector("#numberP1V").value;
+  // input values > player 1 & 2 > name & number
+  let nameP1V = document.querySelector("#nameP1V").value;
+  let numberP1V = document.querySelector("#numberP1V").value;
 
-let nameP2V = document.querySelector("#nameP2V").value;
-let numberP2V = document.querySelector("#numberP2V").value;
+  let nameP2V = document.querySelector("#nameP2V").value;
+  let numberP2V = document.querySelector("#numberP2V").value;
 
-// 
-  saveUserData(nameP1V, numberP1V, nameP2V, numberP2V);
+  // push them in the array
+  addInLS(nameP1V, numberP1V, nameP2V, numberP2V);
 
 });
 
+function fromLS() {
+  // later on registrations
+  let gamesLS = JSON.parse(localStorage.getItem("games"));
+  let playersLS = JSON.parse(localStorage.getItem("players"));
 
-function saveUserData(nameP1V, numberP1V, nameP2V, numberP2V) {
-
-    // later on registrations
-    let gamesLS = JSON.parse(localStorage.getItem("games"));
-    let playersLS = JSON.parse(localStorage.getItem("players"));
-
-    // for the first registration
-    if (gamesLS == null || playersLS == null) {
-// make an array in local storage
-      localStorage.setItem("games", "[]");
+  // for the first registration
+  if (gamesLS == null || playersLS == null) {
+    // make an array in local storage
+    localStorage.setItem("games", "[]");
     //   take that array out of local storage
-      gamesLS = JSON.parse(localStorage.getItem("games"));
+    gamesLS = JSON.parse(localStorage.getItem("games"));
 
-      localStorage.setItem("players", "[]");
-      playersLS = JSON.parse(localStorage.getItem("players"));
-    }
+    localStorage.setItem("players", "[]");
+    playersLS = JSON.parse(localStorage.getItem("players"));
+  }
+  // more than one value is needed so we put them in an array
+  return [gamesLS, playersLS];
+}
 
-// push them in the array
+function addInLS(nameP1V, numberP1V, nameP2V, numberP2V) {
+  // take that array out of local storage or build one if there is not any
+  gamesPlayersLS = fromLS();
+  gamesLS = gamesPlayersLS[0];
+  playersLS = gamesPlayersLS[1];
+
+  // push them in the array
   gamesLS.push({ player1: nameP1V, player2: nameP2V });
+
   playersLS.push(
-    { name: nameP1V, number: numberP1V, win:0, lose:0 },
-    { name: nameP2V, number: numberP2V, win:0, lose:0 }
+    { name: nameP1V, number: numberP1V, win: 0, lose: 0 },
+    { name: nameP2V, number: numberP2V, win: 0, lose: 0 }
   );
 
-//   then put the array back in local storage
+  toLS(gamesLS, playersLS);
+}
+
+function toLS(gamesLS, playersLS) {
+  //   then put the array back in local storage
   localStorage.setItem("games", JSON.stringify(gamesLS));
   localStorage.setItem("players", JSON.stringify(playersLS));
   // add location for btn
-  location.href = "../index.html"
- 
-
+  location.href = "../index.html";
 }
-
-
-
-
 
 // ................................................................
